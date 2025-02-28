27926639d9c80bdbbd86d29458c255cb574894e2
Vikirepo <150894492+Vikirepo@users.noreply.github.com>
2025-02-28 15:57:21 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,14 +1,5 @@
+#!/bin/bash
 set -e
 
 # Stop the running container (if any)
 echo "Hi"
-
-# Get the first container ID
-containerid=$(docker ps -q)
-
-# Check if containerid is not empty before removing
-if [ -n "$containerid" ]; then
-    docker rm -f $containerid
-else
-    echo "No running containers found."
-fi
